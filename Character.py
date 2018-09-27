39522f5998b8c75b0199488c20bb6f2223fbdbc1
Rebekah <40521349+codewraith@users.noreply.github.com>
2018-09-27 15:51:43 -0700

    added a way to vary text descriptions

--- Character.py
+++ Character.py
@@ -8,6 +8,7 @@ Created on Tue Sep 25 10:41:13 2018
 class Adventurer:
     
     def __init__(self):
+        self.name = "Purple Vengeance"
         self.hp = 80
         self.weapon = "+1 Sword of Peskiness"
         self.atkBonus = 1
@@ -17,5 +18,25 @@ class Adventurer:
         self.damageBonus = 2
         self.init = -1
         self.treasure = 0
-        
+        self.atkSuccess = False
+        self.atkDamage = -1
+        self.objType = "player"
+        self.lastGaspThreshold = 1
+        self.strings = { "encounterScream" : { "no" : '"NO!!!" like your mommy taught you',
+                                                "wtf" : "out a mighty battle cry -- at least, " \
+                                                + "that was the plan, but instead you burp.",
+                                                "ew":'"Gross!! Stay away from me!"',
+                                                "lj":'"Leeeeeeroooy Jeeenkiiiiins!!!!!'
+                                                },
+                         "taunt": "Your mother was a hamster and your father smelt of elderberries!"}
 
+"""    @property
+    def getString(self):
+        return self.__getString
+
+    @getString.setter
+    def getString(self,getString):
+        import random
+        choose = self.strings[getString]
+        key = random.choice(choose)
+        self.__getString = choose[key] """
